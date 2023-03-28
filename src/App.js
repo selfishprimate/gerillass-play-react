8c7f0181ca50e79749eb381c1f9ee3bb32164258
Halil İbrahim Çakıroğlu <ibrahimchll@hotmail.com>
2023-03-28 16:14:04 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -3,11 +3,28 @@ import "./style.scss";
 export default function App() {
   return (
     <div className="main-container">
-      <h1>Hello CodeSandbox for Gerillass</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <p class="par">
-        Lorem ipsum is placeholder text commonly used in the graphic, print, and
-        publishing industries for previewing layouts and visual mockups.
+      <h1>Gerillass Sandbox</h1>
+      <h2>
+        Get started with Gerillass and Sass quickly with this sandbox
+        repository!
+      </h2>
+      <p>
+        Gerillass is a lightweight Sass library that helps you build modern and
+        beautiful web designs easily.
+      </p>
+      <p>
+        This repository provides a ready-to-use environment with Sass
+        integration, allowing you to quickly experiment with Gerillass and
+        customize it to your liking.
+      </p>
+      <p>
+        The CodeSandbox implementation is a great way to quickly get started
+        with using Gerillass and Sass for web design projects. With the built-in
+        CodeSandbox environment, users can start modifying the their Sass files
+        right away without the need for any additional setup. The real-time
+        preview pane in CodeSandbox also allows users to see their changes as
+        they're making them, which is a great way to experiment with different
+        design ideas.
       </p>
     </div>
   );
