d4520e0c3d49562e921ef020e25b3f1f1b022f18
Halil İbrahim Çakıroğlu <ibrahimchll@hotmail.com>
2023-03-28 12:46:07 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./style.scss";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <p class="par">
+        Lorem ipsum is placeholder text commonly used in the graphic, print, and
+        publishing industries for previewing layouts and visual mockups.
+      </p>
+    </div>
+  );
+}
